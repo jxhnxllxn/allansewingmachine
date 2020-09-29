efe76ccd47797f6b3fbe48fc479edb15de5d8076
John Allen Belen de Chavez <johnallendechavez23@gmail.com>
2020-09-30 06:15:53 +0800

    server changes

--- server.js
+++ server.js
@@ -47,6 +47,14 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 })
 
+
+// default
+if(process.env.NODE_ENV === 'production'){
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
+
 // Dev logging middleware;
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
