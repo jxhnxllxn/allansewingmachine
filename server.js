4c4fb2ed3c113b93817073ca0bd151aaf4b31a23
John Allen Belen de Chavez <johnallendechavez23@gmail.com>
2020-09-30 06:44:38 +0800

    update

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ cloudinary.config({
 
 // default
 if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
     app.get('/*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
     })
