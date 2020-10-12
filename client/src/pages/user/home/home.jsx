6e21a44a157068ac6cde8aff5636d13675b62981
John Allen Belen de Chavez <johnallendechavez23@gmail.com>
2020-10-12 08:43:07 +0800

    fixing errors

--- client/src/pages/user/home/home.jsx
+++ client/src/pages/user/home/home.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 import HomeSlider from './home-slider/home-slider';
 import CardBlock from '../../../components/utils/card-block/card-block';
 
-import {store} from "../../../redux/store";
-import { getProductsByArrival,getProductsBySell} from "../../../redux/product/product-action";
+import { store } from "../../../redux/store";
+import { getProductsByArrival, getProductsBySell } from "../../../redux/product/product-action";
 import './home.scss'
 
 const Home = () => {
 
 
-     useEffect(() => {
+    useEffect(() => {
         store.dispatch(getProductsByArrival());
         store.dispatch(getProductsBySell());
     }, []);
@@ -19,18 +19,18 @@ const Home = () => {
     const productBySell = useSelector(state => state.product.productBySell)
     const productByArrival = useSelector(state => state.product.productByArrival)
 
-    
+
     // const products = useSelector(state => state.product.products)
 
     return (
         <div className="home_wrapper">
-            <HomeSlider />
-            <CardBlock 
-                list={productBySell} 
+            {/* <HomeSlider /> */}
+            <CardBlock
+                list={productBySell}
                 title="Best Seller"
             />
-            <CardBlock 
-                list={productByArrival} 
+            <CardBlock
+                list={productByArrival}
                 title="New Arrival"
             />
         </div>
