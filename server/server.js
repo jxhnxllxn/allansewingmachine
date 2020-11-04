9686e5775f18e422867ef85de8d004dc87fd4c80
John Allen Belen de Chavez <johnallendechavez23@gmail.com>
2020-11-04 16:39:36 +0800

    server.js update

--- server/server.js
+++ server/server.js
@@ -22,11 +22,16 @@ if (process.env.NODE_ENV === 'development') {
 
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '/client/build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    })
+    
+} else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
 }
 
 const PORT = process.env.PORT || 5000;
